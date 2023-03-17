ba1a19330edfffac44264a31f821b8fffc86a14a
nico <westernfarthing@gmail.com>
2023-03-17 01:35:40 -0500

    fixed db errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,48 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log("PUT to the database");
+  // Create a connection to the database database and version we want to use.
+  const todosDb = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = todosDb.transaction("jate", "readwrite");
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+  // Use the .put() method to get all data in the database.
+  const request = store.put({ content: content });
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET all from the database");
+  // Create a connection to the database database and version we want to use.
+  const todosDb = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = todosDb.transaction("jate", "readonly");
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result);
+  return result.content;
+};
 
 initdb();
